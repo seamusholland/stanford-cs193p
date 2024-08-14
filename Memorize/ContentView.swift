1d4090ebe9bb10caa76c339d362f250ee6e99beb
seamusholland <martinholland97@gmail.com>
2024-08-14 10:21:02 -0400

    Togglable isFaceUp

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -21,19 +21,23 @@ struct ContentView: View {
 }
 
 struct CardView: View {
-    var isFaceUp: Bool = false
+    @State var isFaceUp = false
     
     var body: some View {
         ZStack {
+            let base = RoundedRectangle(cornerRadius: 12)
+            
             if (isFaceUp) {
-                RoundedRectangle(cornerRadius: 12)
-                    .foregroundColor(.white)
-                RoundedRectangle(cornerRadius: 12)
+                base
+                    .fill(.white)
+                base
                     .strokeBorder(lineWidth: 2)
                 Text("🦔").font(.largeTitle)
             } else {
-                RoundedRectangle(cornerRadius: 12)
+                base.fill()
             }
+        }.onTapGesture {
+            isFaceUp.toggle()
         }
     }
 }
